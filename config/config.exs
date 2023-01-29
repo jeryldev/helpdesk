b882888691bf9a16a72b659ad6e47f4b823b37fa
jeryldev <jeryldev@gmail.com>
2023-01-29 22:37:29 +0800

    add helpdesk resource

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,6 @@ import Config
 
 # if you already have a config file, then you'll just need to add this:
 config :ash, :use_all_identities_in_manage_relationship?, false
+
+# Ash APIs
+config :helpdesk, :ash_apis, [Helpdesk.Support]
