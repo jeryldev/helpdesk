c3571b01871de12b4114857481386fa359131801
jeryldev <jeryldev@gmail.com>
2023-02-02 10:02:34 +0800

    create and configure Helpdesk.Repo

--- config/config.exs
+++ config/config.exs
@@ -5,3 +5,10 @@ config :ash, :use_all_identities_in_manage_relationship?, false
 
 # Ash APIs
 config :helpdesk, :ash_apis, [Helpdesk.Support]
+
+# Ash Postgres
+config :helpdesk, ecto_repos: [Helpdesk.Repo]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
